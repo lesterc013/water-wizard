b5fbd1fce70cde4dc3a565076480bee8e4d12ebc
Lester Chan <lesterc013@gmail.com>
2024-03-12 22:30:34 +0800

    included basic chrome local storage api. refactored button click functionalities

--- script.js
+++ script.js
@@ -4,27 +4,47 @@ const cupsDrankElement = document.getElementById("cupsDrank");
 const cupsLeftElement = document.getElementById("cupsLeft");
 const idealCups = 8;
 
+// persisting user data in local storage. also serves as logic on how to display default html
+chrome.storage.local.get("counter", function (result) {
+  // callback function essentially tells me if counter was logged already or not
+  // if user has not logged in before:
+  if (result["counter"] === undefined) {
+    console.log("User first time");
+  }
+  // user has some logs
+  else {
+    console.log("User has data stored");
+    chrome.storage.local.get("counter", function (result) {
+      console.log("Value is " + result.counter);
+    });
+  }
+});
+
 let counter = 0;
 // set default value for cupsDrank element
 cupsDrankElement.innerHTML = counter;
 // set default value for cupsLeft element
 cupsLeftElement.innerHTML = `Bibbity Boppity, you have ${idealCups} cups left to drinkity! Keepity Uppity!`;
 
-addButton.onclick = function () {
+addButton.onclick = addButtoncClick;
+
+subtractButton.onclick = subtractButtonClick;
+
+function addButtoncClick() {
   // add 1 to the counter. then update cupsDrankElement
   counter += 1;
   cupsDrankElement.innerHTML = counter;
   displayCupsLeft();
-};
+}
 
-subtractButton.onclick = function () {
+function subtractButtonClick() {
   // subtract 1 to the counter. then update cupsDrankElement. only allow subtraction if counter > 0 i.e. no negatives
   if (counter > 0) {
     counter -= 1;
     cupsDrankElement.innerHTML = counter;
     displayCupsLeft();
   }
-};
+}
 
 function displayCupsLeft() {
   // if user still hasnt reached idealCups i.e. 8, display this message
