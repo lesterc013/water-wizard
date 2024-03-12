97df037f646855e59be8c19f543bc84d42deffe7
Lester Chan <lesterc013@gmail.com>
2024-03-13 00:07:47 +0800

    added functionality to persist user's drinks

--- script.js
+++ script.js
@@ -4,41 +4,55 @@ const cupsDrankElement = document.getElementById("cupsDrank");
 const cupsLeftElement = document.getElementById("cupsLeft");
 const idealCups = 8;
 
+// default value of counter - declared gloablly to allow access
+let counter = 0;
+
 // persisting user data in local storage. also serves as logic on how to display default html
 chrome.storage.local.get("counterKey", function (result) {
   // callback function essentially tells me if counter was logged already or not
   // if user has not logged in before:
   if (result["counterKey"] === undefined) {
     console.log("User first time");
-    let counter = 0;
-    // set the counterKey to the initialized value
+    // set the counterKey to the default value of counter i.e. 0
     chrome.storage.local.set({ counterKey: counter }, function () {
       console.log("counterKey value set to " + counter);
     });
+    // set default value for cupsDrank element
+    cupsDrankElement.innerHTML = counter;
+    // set default value for cupsLeft element
+    cupsLeftElement.innerHTML = `Bibbity Boppity, you have ${idealCups} cups left to drinkity! Keepity Uppity!`;
+
+    // add and subtract buttons functionalities
+    addButton.onclick = addButtonClick;
+    subtractButton.onclick = subtractButtonClick;
   }
   // user has some logs
   else {
     console.log("User has data stored");
     chrome.storage.local.get("counterKey", function (result) {
       console.log("Value is " + result.counterKey);
+      // update the value of counter to what was stored
+      counter = result.counterKey;
+      // value for cupsDrank
+      cupsDrankElement.innerHTML = counter;
+      // value for cupsLeft
+      displayCupsLeft();
+
+      // add and subtract buttons functionalities
+      addButton.onclick = addButtonClick;
+      subtractButton.onclick = subtractButtonClick;
     });
   }
 });
-
-let counter = 0;
-// set default value for cupsDrank element
-cupsDrankElement.innerHTML = counter;
-// set default value for cupsLeft element
-cupsLeftElement.innerHTML = `Bibbity Boppity, you have ${idealCups} cups left to drinkity! Keepity Uppity!`;
-
-addButton.onclick = addButtonClick;
-
-subtractButton.onclick = subtractButtonClick;
 
 function addButtonClick() {
   // add 1 to the counter. then update cupsDrankElement
   counter += 1;
   cupsDrankElement.innerHTML = counter;
+  // update the value of storage with current counter value
+  chrome.storage.local.set({ counterKey: counter }, function () {
+    console.log("counterKey value set to " + counter);
+  });
   displayCupsLeft();
 }
 
@@ -47,6 +61,10 @@ function subtractButtonClick() {
   if (counter > 0) {
     counter -= 1;
     cupsDrankElement.innerHTML = counter;
+    // update the value of storage with current counter value
+    chrome.storage.local.set({ counterKey: counter }, function () {
+      console.log("counterKey value set to " + counter);
+    });
     displayCupsLeft();
   }
 }
