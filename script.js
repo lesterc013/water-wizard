bf7192a8718b2c450cb0f928e252717fdd94f1d4
Lester Chan <lesterc013@gmail.com>
2024-03-12 23:53:19 +0800

    added basic chrome storage api functionality for first time user and if user already had data input

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ const cupsLeftElement = document.getElementById("cupsLeft");
 const idealCups = 8;
 
 // persisting user data in local storage. also serves as logic on how to display default html
-chrome.storage.local.get("counter", function (result) {
+chrome.storage.local.get("counterKey", function (result) {
   // callback function essentially tells me if counter was logged already or not
   // if user has not logged in before:
-  if (result["counter"] === undefined) {
+  if (result["counterKey"] === undefined) {
     console.log("User first time");
+    let counter = 0;
+    // set the counterKey to the initialized value
+    chrome.storage.local.set({ counterKey: counter }, function () {
+      console.log("counterKey value set to " + counter);
+    });
   }
   // user has some logs
   else {
     console.log("User has data stored");
-    chrome.storage.local.get("counter", function (result) {
-      console.log("Value is " + result.counter);
-    });
-  }
+    chrome.storage.local.get("counterKey", function (result) {
+      console.log("Value is " + result.counterKey);
+    })
 });
 
 let counter = 0;
