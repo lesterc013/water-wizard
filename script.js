31937ca9b5a7f2f42260980fa2491063c55d2dac
Lester Chan <lesterc013@gmail.com>
2024-03-12 17:18:48 +0800

    Included rudimentary counter, add and subtract functionalities

---
+++ script.js
@@ -0,0 +1,19 @@
+const addButton = document.getElementById("add");
+const subtractButton = document.getElementById("subtract");
+const counterElement = document.getElementById("counter");
+
+let counter = 0;
+// set default value for counter element
+counterElement.innerHTML = counter;
+
+// add 1 to the counter element
+addButton.onclick = function () {
+  counter += 1;
+  counterElement.innerHTML = counter;
+};
+
+// subtract 1 to the counter element
+subtractButton.onclick = function () {
+  counter -= 1;
+  counterElement.innerHTML = counter;
+};
