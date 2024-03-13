86214da32abad3428fae7ed4a77bc43da3ca9f93
Lester Chan <lesterc013@gmail.com>
2024-03-13 21:45:12 +0800

    added feature to display images wrt number of cups drank

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addButton = document.getElementById("add");
 const subtractButton = document.getElementById("subtract");
 const cupsDrankElement = document.getElementById("cupsDrank");
 const cupsLeftElement = document.getElementById("cupsLeft");
+const cupImagesDiv = document.getElementById("cupImages");
 const idealCups = 8;
 
 // default value of counter - declared gloablly to allow access
@@ -17,11 +18,15 @@ chrome.storage.local.get("counterKey", function (result) {
     chrome.storage.local.set({ counterKey: counter }, function () {
       console.log("counterKey value set to " + counter);
     });
-    // set default value for cupsDrank element
-    cupsDrankElement.innerHTML = counter;
     // set default value for cupsLeft element
     cupsLeftElement.innerHTML = `Bibbity Boppity, you have ${idealCups} cups left to drinkity! Keepity Uppity!`;
-
+    // set default value for cupsDrank element
+    cupsDrankElement.innerHTML = counter;
+    // // default value for cupsImages div
+    // const image =
+    //   '<img src="images/dead-water-wizard-128.png" alt="dead-wizard">';
+    // cupImagesDiv.innerHTML += image;
+    displayCupImages();
     // add and subtract buttons functionalities
     addButton.onclick = addButtonClick;
     subtractButton.onclick = subtractButtonClick;
@@ -33,11 +38,12 @@ chrome.storage.local.get("counterKey", function (result) {
       console.log("Value is " + result.counterKey);
       // update the value of counter to what was stored
       counter = result.counterKey;
-      // value for cupsDrank
-      cupsDrankElement.innerHTML = counter;
       // value for cupsLeft
       displayCupsLeft();
-
+      // value for cupsDrank
+      cupsDrankElement.innerHTML = counter;
+      // display initial cupImages
+      displayCupImages();
       // add and subtract buttons functionalities
       addButton.onclick = addButtonClick;
       subtractButton.onclick = subtractButtonClick;
@@ -54,6 +60,7 @@ function addButtonClick() {
     console.log("counterKey value set to " + counter);
   });
   displayCupsLeft();
+  displayCupImages();
 }
 
 function subtractButtonClick() {
@@ -66,6 +73,7 @@ function subtractButtonClick() {
       console.log("counterKey value set to " + counter);
     });
     displayCupsLeft();
+    displayCupImages();
   }
 }
 
@@ -77,5 +85,21 @@ function displayCupsLeft() {
     } cups left to drinkity! Keepity Uppity!`;
   } else {
     cupsLeftElement.innerHTML = `Yippee Yahooity, you are sufficiently hydratedty! Well Doneity and Keepity Uppity!`;
+  }
+}
+
+function displayCupImages() {
+  let images = "";
+  // if the counter is 0 - set to dead-water-wizard
+  if (counter == 0) {
+    images = '<img src="images/dead-water-wizard-128.png" alt="dead-wizard">';
+    cupImagesDiv.innerHTML = images;
+  }
+  // else, append how many number of water wizard images based on the counter value. add condition to only append up to 8 number of water-wizards
+  else {
+    for (let i = 0; i < counter && i <= 7; i++) {
+      images += '<img src="images/icon-48.png" alt="water-wizard">';
+    }
+    cupImagesDiv.innerHTML = images;
   }
 }
