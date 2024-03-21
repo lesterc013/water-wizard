a7e82bd538d6304ea658a926c2d63edd6eb03a26
Lester Chan <lesterc013@gmail.com>
2024-03-22 00:01:39 +0800

    changed using setTimeout to chrome alarms api as manifest v3 kills off service worker thus setTimeout not ideal. alarms persists even if browser is closed

--- timeToClear.js
+++ timeToClear.js
@@ -1,28 +1,35 @@
-// background script that sets the time to clear the local storage with respect to 0000 of the next day. Much help from chatGPT on this
+// change setTimeout to alarms so that alarms can clear the storage even when browser is closed + not be affected by service worker terminating
+// create the alarm
+const now = new Date();
+const clearDate = new Date(now);
+// set clearDate to be one day after now
+clearDate.setDate(now.getDate() + 1);
+// set clearDate time to be 0000
+clearDate.setHours(0, 0, 0, 0);
+// milliseconds from now to clearDate
+const timeToClear = clearDate - now;
 
-scheduleClear();
+// clear any previous alarms
+chrome.alarms.clearAll();
+
+// create the alarm that will fire off within timeToClear
+alarmInfo = {
+  when: Date.now() + timeToClear,
+};
+chrome.alarms.create("clearStorageAlarm", alarmInfo);
+console.log("Alarm created");
+console.log("Time to clear ", timeToClear);
+
+// now create the alarm listener that will listen for the alarm name from on top. once alarm fires, execute the clearStorage function
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  console.log("Alarm fired", alarm);
+  if (alarm.name === "clearStorageAlarm") {
+    clearStorage();
+  }
+});
 
 // function to clear the storage
 function clearStorage() {
   chrome.storage.local.clear(function () {});
-
-  // after clearing, call scheduleClear to ensure continuity
-  scheduleClear();
-}
-
-// function to schedule the clearStorage function
-function scheduleClear() {
-  // the date now
-  const now = new Date();
-
-  // clear the storage at 0000 of the next day from now
-  const clearDate = new Date(now);
-  // set clearDate to be one day after now
-  clearDate.setDate(now.getDate() + 1);
-  // set clearDate time to be 0000
-  clearDate.setHours(0, 0, 0, 0);
-  // milliseconds from now to clearDate
-  const timeToClear = clearDate - now;
-  // set the time to clear the storage
-  setTimeout(clearStorage, timeToClear);
+  console.log("Storage cleared at 0000");
 }
