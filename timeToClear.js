0e3ac42ccb589b322fb90a7870b2387a9ec00ed4
lesterchan <lesterc013@gmail.com>
2024-03-29 00:05:00 +0800

    fixed bug where storage would not clear if user totally quits chrome

--- timeToClear.js
+++ timeToClear.js
@@ -1,8 +1,24 @@
 // on chrome startup, create an alarm
 chrome.runtime.onStartup.addListener(function () {
-  const timeToClear = setTimeToClear();
-  // create alarm
-  createAlarm(timeToClear);
+  if (checkPastMidnight()) {
+    console.log("Past midnight");
+    checkIfStoredDataFromPreviousDay(function (lessThanMidnightOfToday) {
+      if (lessThanMidnightOfToday) {
+        console.log("Past midnight and data was from previous day");
+        clearStorage();
+        const timeToClear = setTimeToClear();
+        createAlarm(timeToClear);
+      } else {
+        console.log("Past midnight but data from current day");
+        const timeToClear = setTimeToClear();
+        createAlarm(timeToClear);
+      }
+    });
+  } else {
+    console.log("Not past midnight");
+    const timeToClear = setTimeToClear();
+    createAlarm(timeToClear);
+  }
 });
 
 // create alarm listener
@@ -44,5 +60,35 @@ function createAlarm(timeToClear) {
 // function to clear the storage
 function clearStorage() {
   chrome.storage.local.clear(function () {});
-  console.log("Storage cleared at 0000");
+  console.log("Storage cleared");
+}
+
+// function to checkIfPastMidnight
+function checkPastMidnight() {
+  const now = new Date();
+  console.log("now hours " + now.getHours());
+  console.log("now minutes " + now.getMinutes());
+  if (now.getHours() == 0 && now.getMinutes() == 0) {
+    return true;
+  } else return false;
+}
+
+// check if the time of the inputs are less than the 0000 of today
+function checkIfStoredDataFromPreviousDay(callback) {
+  // - get a variable that contains the 0000 of today
+  const now = new Date();
+  const midnightOfToday = Date.now(now.setHours(0, 0, 0, 0));
+  console.log("midnight of today is " + midnightOfToday);
+  let lessThanMidnightOfToday = false;
+  // - get the timestamps of all the inputs in the storage
+  chrome.storage.local.get("counterKey", function (result) {
+    // get timestamp of the counterValue
+    const timestamp = result.counterKey.timestamp;
+    console.log("timestamp for data is " + timestamp);
+    if (timestamp < midnightOfToday) {
+      lessThanMidnightOfToday = true;
+    }
+    console.log(lessThanMidnightOfToday);
+    callback(lessThanMidnightOfToday);
+  });
 }
